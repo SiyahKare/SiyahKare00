d471ff3606a55c9382d927214535739ecb940fd3
ebuer <eryigitdev@gmail.com>
2020-11-16 17:35:14 +0300

    change index.php

--- static/api/index.php
+++ static/api/index.php
@@ -1,4 +1,12 @@
 <?php
+
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
+
+
 header('Content-Type: application/json');
 require ('Libs/Validation.php');
 require ('Libs/MailSend.php');
