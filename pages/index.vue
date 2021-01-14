51e9ba82c39450dfca29776f5e1c3223d3539290
ebuer <eryigitdev@gmail.com>
2021-01-14 21:09:39 +0300

    edit

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
 
     <client-only>
       <div style="padding:56.25% 0 0 0;position:relative;">
-        <iframe src="https://player.vimeo.com/video/500399158?autoplay=1&loop=1&title=0&byline=0&portrait=0"
+        <iframe src="https://player.vimeo.com/video/500399158?autoplay=1&loop=1&autopause=0&title=0&byline=0&portrait=0"
                 style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0"
                 allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
       </div>
